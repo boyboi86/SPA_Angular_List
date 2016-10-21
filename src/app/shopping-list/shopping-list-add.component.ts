ae8293800e3b761c8395079853b74313f070b5a3
boyboi86 <boyboi86@gmail.com>
2016-10-21 21:39:01 +0800

    added new method onSubmit

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'rb-shopping-list-add',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListAddComponent implements OnInit {
   isAdd = true;
-  
-  constructor() {}
+  item: Ingredient;
+  constructor(private sls: ShoppingListService) {}
 
   ngOnInit() {
   }
 
+  onSubmit(ingredient: Ingredient){
+    if(!this.isAdd){
+      //edit
+    } else {
+      this.item = new Ingredient(ingredient.name, ingredient.amount);
+      this.sls.addItem(this.item);
+    }
+  }
 }
