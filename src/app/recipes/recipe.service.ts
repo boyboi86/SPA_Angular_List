4c9829a640c99ecdb99ebcbf4c160f9e487ac371
boyboi86 <boyboi86@gmail.com>
2016-10-23 19:37:58 +0800

    update new services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -5,6 +5,7 @@ import { Ingredient } from "../shared";
 
 @Injectable()
 export class RecipeService {
+
   private recipes: Recipe[] = [
     new Recipe('Schnitzel', 'Very tasty', 'http://images.derberater.de/files/imagecache/456xXXX_berater/berater/slides/WienerSchnitzel.jpg', [
       new Ingredient('French Fries', 2),
@@ -18,13 +19,21 @@ export class RecipeService {
   getRecipes() {
     return this.recipes;
   }
-  
+
   getRecipe(id: number) {
     return this.recipes[id];
   }
-  
+
   deleteRecipe(recipe: Recipe) {
     this.recipes.splice(this.recipes.indexOf(recipe), 1);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+  }
+
+  editRecipe(oldRecipe: Recipe, newRecipe: Recipe) {
+    this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
   }
 
 }
