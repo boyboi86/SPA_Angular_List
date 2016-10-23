1005681f462eced5cd74114be2c735ead7688599
boyboi86 <boyboi86@gmail.com>
2016-10-23 19:46:57 +0800

    shopping list service update

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -12,12 +12,17 @@ export class ShoppingListService {
   addItems(items: Ingredient[]) {
     Array.prototype.push.apply(this.items, items);
   }
-
-  addItem(item: Ingredient){
+  
+  addItem(item: Ingredient) {
     this.items.push(item);
   }
-
-  editItem(oldItem: Ingredient, newItem: Ingredient){
+  
+  editItem(oldItem: Ingredient, newItem: Ingredient) {
     this.items[this.items.indexOf(oldItem)] = newItem;
-  };
+  }
+  
+  deleteItem(item: Ingredient) {
+    this.items.splice(this.items.indexOf(item), 1);
+  }
+
 }
