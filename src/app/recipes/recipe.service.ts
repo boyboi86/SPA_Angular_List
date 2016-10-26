40502fafca5ea770f19b20041391716a8f534874
boyboi86 <boyboi86@gmail.com>
2016-10-26 16:46:36 +0800

    changed POST to PUT

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,13 @@
-import { Injectable } from '@angular/core';
-import { Headers, Http } from '@angular/http';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
 
 import { Recipe } from "./recipe";
 import { Ingredient } from "../shared";
+import 'rxjs/Rx';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new EventEmitter<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe('Schnitzel', 'Very tasty', 'http://images.derberater.de/files/imagecache/456xXXX_berater/berater/slides/WienerSchnitzel.jpg', [
@@ -36,17 +38,24 @@ export class RecipeService {
   editRecipe(oldRecipe: Recipe, newRecipe: Recipe) {
     this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
   }
-
+/*PUT is used in place of POST due to firebase problem*/
   storeData(){
     const body = JSON.stringify(this.recipes);
     const headers = new Headers({
       'Content-Type': 'application/json'
     });
-    return this.http.post('https://recipebook-bb148.firebaseio.com/recipes.json', body, {headers: headers})
+    return this.http.put('https://recipebook-bb148.firebaseio.com/recipes.json', body, {headers: headers})
   }
-
+/*map is from rxjs*/
   fetchData(){
-
+    return this.http.get('https://recipebook-bb148.firebaseio.com/recipes.json')
+                    .map((response: Response)=> response.json())
+                    .subscribe(
+                      (data: Recipe[]) => {
+                        this.recipes = data;
+                        this.recipesChanged.emit(this.recipes)
+                      }
+                    );
   }
 
 }
